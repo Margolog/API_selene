aae6a9734a45cb0b0c7316c2129e951f64609951
Logunova Margarita <logunovar@gmail.com>
2022-12-13 23:35:10 +0300

    new

--- tests/tests.py
+++ tests/tests.py
@@ -15,12 +15,13 @@ def test_create_user(reqres_session, lambda_steps):
 
     result: Response = reqres_session.post(url='/api/users',
                                            json={"name": name, "job": job})
-
-    assert result.status_code == 201
-    assert result.json()['name'] == name
-    assert result.json()['job'] == job
-    assert isinstance(result.json()['id'], str)
-    assert result.json() == S(create_user)
+    with allure.step('Проверка статус кода'):
+        assert result.status_code == 201
+    with allure.step('Проверка полей в ответе'):
+        assert result.json()['name'] == name
+        assert result.json()['job'] == job
+    with allure.step('Проверка архитектуры ответа'):
+        assert result.json() == S(create_user)
 
 
 @allure.epic('Test API')
@@ -33,11 +34,13 @@ def test_update_user(reqres_session):
 
     result: Response = reqres_session.put(url='/api/users/2',
                                           json={"name": name, "job": job})
-
-    assert result.status_code == 200
-    assert result.json()['name'] == name
-    assert result.json()['job'] == job
-    assert result.json() == S(update_user)
+    with allure.step('Проверка статус кода'):
+        assert result.status_code == 200
+    with allure.step('Проверка полей в ответе'):
+        assert result.json()['name'] == name
+        assert result.json()['job'] == job
+    with allure.step('Проверка архитектуры ответа'):
+        assert result.json() == S(update_user)
 
 
 @allure.epic('Test API')
@@ -47,8 +50,10 @@ def test_update_user(reqres_session):
 def test_user_not_found(reqres_session, lambda_steps):
     result: Response = reqres_session.get(url='/api/unknown/23')
 
-    assert result.status_code == 404
-    assert result.json() == S(user_not_found)
+    with allure.step('Проверка статус кода'):
+        assert result.status_code == 404
+    with allure.step('Проверка архитектуры ответа'):
+        assert result.json() == S(user_not_found)
 
 
 @allure.epic('Test API')
@@ -58,7 +63,8 @@ def test_user_not_found(reqres_session, lambda_steps):
 def test_delete_user(reqres_session,lambda_steps):
     result = reqres_session.delete(url='/api/users/2')
 
-    assert result.status_code == 204
+    with allure.step('Проверка статус кода'):
+        assert result.status_code == 204
 
 
 @allure.epic('Test API')
@@ -71,5 +77,7 @@ def test_register_unsuccessful(reqres_session):
     result: Response = reqres_session.post(url='/api/login',
                                            json={"email": email})
 
-    assert result.status_code == 400
-    assert result.json() == S(register_unsuccessful)
+    with allure.step('Проверка статус кода'):
+        assert result.status_code == 400
+    with allure.step('Проверка архитектуры ответа'):
+        assert result.json() == S(register_unsuccessful)
